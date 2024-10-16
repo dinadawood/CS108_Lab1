c5a96532f758492956f816f85c8c775339c18926
Dina Dawood <ddawood@udel.edu>
2024-10-16 12:31:04 -0400

    Add files via upload

---
+++ CISC108 - Lab_1_Dawood_D.py
@@ -0,0 +1,93 @@
+"""
+Course: CISC108
+Assignment: Lab 1
+Author: Dina Dawood
+"""
+
+# Problem 1.
+print("wwww")
+
+# Problem 2.
+print("")
+
+# Problem 3.
+print("18 Amstel Ave, Newark, DE 19716")
+print("(302) 831-2792")
+
+# Problem 4.
+sqrt2 = 1.41421
+print(sqrt2)
+
+# Problem 5.
+five = 5
+print(five)
+
+# Problem 6.
+boiling = 212.0
+print(boiling)
+
+# Problem 7.
+suit = str("spades")
+print(suit)
+
+# Problem 8.
+HEIGHT = 84.5
+WEIGHT = 188.0
+print("HEIGHT = ", HEIGHT)
+print("WEIGHT = ", WEIGHT)
+
+# Problem 9.
+frosh_age = 20
+grad_age = frosh_age + 4
+print("Grad age = ", grad_age)
+
+# Problem 10.
+foodCost = 16
+drinkCost = 3
+mealCost = foodCost + drinkCost
+print("Meal Cost = $", mealCost)
+
+# Problem 11.
+numTickets = 200
+price = 45.00
+totalCost = numTickets * price
+print("Total = $", totalCost)
+
+# Problem 12.
+startTime = 0.02
+endTime = 0.59
+elapsedTime = endTime - startTime
+print("Total Time = ", elapsedTime)
+
+# Problem 13.
+length1 = 11
+width1 = 7
+length2 = 12
+width2 = 4
+totalArea = (length1 * width1) + (length2 * width2)
+print("Total Area = ", totalArea)
+
+# Problem 14.
+firstInClass = "Pat"
+secondInClass = "Wei"
+swap = firstInClass
+firstInClass = secondInClass
+print(firstInClass)
+print(swap)
+
+# Problem 15.A.
+data_1 = 8.5
+print(data_1)
+
+# Problem 15.B.
+data_2 = 2.3
+print(data_2)
+
+# Problem 15.C.
+data_3 = 3.4
+print(data_3)
+
+# Problem 15.D.
+avg_data = (data_1 + data_2 + data_3)/3
+print(avg_data)
+
